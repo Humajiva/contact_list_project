031e96da5e2f1f11f659302a6e47b39b317cae94
George Mihov <gmihov001@gmail.com>
2022-02-24 13:10:30 +0000

    Initial

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,31 @@
+import React, { useState, useEffect } from "react";
+
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
 			//Your data structures, A.K.A Entities
+			agenda: []
 		},
 		actions: {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
+			getData: () => {
+				fetch("https://assets.breatheco.de/apis/fake/contact/agenda/Huma_jiva")
+					.then(response => {
+						console.log(response);
+						if (!response.ok) {
+							throw Error(response.statusText);
+						}
+						return response.json();
+					})
+					.then(data => {
+						console.log(data);
+						setStore({ agenda: data });
+					})
+					.catch(error => {
+						console.log("error", error);
+					});
+			}
 		}
 	};
 };
